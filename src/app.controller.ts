913ea946c8abcf983a799b0d7a11d7e90c8c5929
jihoon <wlgns2007@gmail.com>
2026-04-27 23:39:07 +0900

    app.controller 오류 해결

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { User } from '@prisma/client';
 
 @Controller()
 export class AppController {
@@ -9,13 +10,15 @@ export class AppController {
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  login(@Request() req) {
+  login(@Request() req: Request & { user: Omit<User, 'password'> }) {
     return this.authService.login(req.user);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
-  getProfile(@Request() req) {
+  getProfile(
+    @Request() req: Request & { user: { userId: number; username: string } },
+  ) {
     return req.user;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService {
     return null;
   }
 
-  login(user: User): { access_token: string } {
+  login(user: Omit<User, 'password'>): { access_token: string } {
     const payload = { username: user.name, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
